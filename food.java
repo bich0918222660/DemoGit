1775b63e36cfb95f437c8dd0685521e8b2405e08
Trieu Bich <tnbichiuh@gmail.com>
2017-12-04 14:14:42 +0700

    change food.java

--- food.java
+++ food.java
@@ -6,6 +6,7 @@ public class Hungry {
 		{
 			System.out.println("Eat hambuger");
 			System.out.println("Eat KFC");
+			System.out.println("Eat Sandwich");
 		}
 		else {
 			System.out.println("OK");
