75f78c475ed4404fefde2d5c182ef4c0a2ff1262
Trieu Bich <tnbichiuh@gmail.com>
2017-12-04 13:12:28 +0700

    add drink

---
+++ food.java
@@ -0,0 +1,19 @@
+public class Hungry {
+
+	public static void main(String[] args) {
+		String hungry = "Are you hungry?";
+		if(hungry.equals("yes"))
+		{
+			System.out.println("Eat hambuger");
+			System.out.println("Eat fries");
+		}
+		else {
+			String drink = "Are you thirsty?"
+			if(drink.equals("yes")
+			{
+				System.out.println("Drink coffee");
+			}
+		}
+	}
+
+}
