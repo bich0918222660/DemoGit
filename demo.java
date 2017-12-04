8c6bb002f32d2d17c8af044808421cbf4c802ae9
Trieu Bich <tnbichiuh@gmail.com>
2017-12-04 11:50:18 +0700

    Drink orange juice

--- demo.java
+++ demo.java
@@ -1,15 +1,25 @@
 public class Hungry {
 
+	
 	public static void main(String[] args) {
+		
 		String hungry = "Are you hungry?";
+		
 		if(hungry.equals("yes"))
 		{
+			
 			System.out.println("Eat hambuger");
+			
 			System.out.println("Eat fries");
+		
 		}
 		else {
-			System.out.println("Ok!");
+			String drink = "Are you thirsty?";
+			System.out.println("Drink orange juice");
+		
 		}
+	
 	}
+
 
 }
