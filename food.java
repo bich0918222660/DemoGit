d42719fe335499a2d1a3d6dee160df386b70703c
Trieu Bich <tnbichiuh@gmail.com>
2017-12-04 13:01:18 +0700

    add food.java

---
+++ food.java
@@ -0,0 +1,14 @@
+public class Hungry {
+
+	public static void main(String[] args) {
+		String hungry = "Are you hungry?";
+		if(hungry.equals("yes"))
+		{
+			System.out.println("Eat hambuger");
+			System.out.println("Eat fries");
+		}
+		System.out.println("Ok!");
+		
+	}
+
+}
